4ef7c566ede4ac0148d42396d1803c8f11c5f177
Mediatec Software <hello@mediatec.org>
2020-05-09 01:03:12 +0300

    dark theme init

--- src/components/assets/index.js
+++ src/components/assets/index.js
@@ -1,10 +1,17 @@
-$(document).ready(function () {
+$(function () {
+  // dblclick change theme
   $(document).dblclick(function () {
     var attr = $('html').attr('data-kit-theme')
     if (attr === 'dark') {
       $('html').attr('data-kit-theme', '')
+      window.localStorage.setItem('data-kit-theme', '')
     } else {
       $('html').attr('data-kit-theme', 'dark')
+      window.localStorage.setItem('data-kit-theme', 'dark')
     }
   })
+
+  // init dark theme 
+  var theme = window.localStorage.getItem('data-kit-theme')
+  $('html').attr('data-kit-theme', theme)
 })
